9f4ff931f0d0c6cb11577a993176b3b803b21156
TereshinaEVa <tereshina.eva@gmail.com>
2024-11-29 16:56:32 +0300

    17_4-Использование БД в маршрутизации. 1.1

--- Module17/app/main.py
+++ Module17/app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
-from routers import task
-from routers import user
+from .routers.task import router as tr
+from .routers.user import router as ur
 
-app = FastAPI()
+app = FastAPI(swagger_ui_parameters={'tryItOutEnabled': True})
 
 
 @app.get('/')
 async def welcome():
     return {"message": "Welcome to Taskmanager"}
 
-app.include_router(task.router)
-app.include_router(user.router)
+app.include_router(tr)
+app.include_router(ur)
--- Module17/app/routers/task.py
+++ Module17/app/routers/task.py
@@ -1,4 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from app.backend.db_depends import get_db
+from typing import Annotated
+from app.models import Task
+from app.schemas import CreateTask, UpdateTask
+from sqlalchemy import insert, select, update, delete
+from slugify import slugify
 
 router = APIRouter(prefix='/task', tags=['task'])
 
@@ -9,7 +16,7 @@ async def all_tasks():
 
 
 @router.get('/task_id')
-async def task_dy_id():
+async def task_by_id():
     pass
 
 
